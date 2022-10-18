2e21f320fe8220cc5c4efdbea60ad0a63454906b
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-18 09:05:09 -0500

    added MongooseConnection and Middlewares

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const helmet = require("helmet");
-
+const MongooseConnection = require("./utility/mongoose.connection");
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
+
+// Create application with express
+const app = express();
+
+// Connect to database
+require("dotenv").config();
+MongooseConnection();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
