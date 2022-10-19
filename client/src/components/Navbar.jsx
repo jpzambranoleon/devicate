ab821dd7b5d42750f057bf6e1d9fade06f6da784
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-19 14:23:28 -0500

    created Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const settings = ["Logout"];
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>Devicate</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
