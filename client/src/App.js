8d1aa3959598ced170729655828b8e682944f7d6
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-19 14:11:45 -0500

    imported useContext and InfoContext

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,29 @@ import { Box, Container, Grid } from "@mui/material";
 import axios from "axios";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
+import { useContext } from "react";
+import { InfoContext } from "./utility/InfoProvider";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:5500/api";
 
+  const { authorized } = useContext(InfoContext);
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      {authorized ? (
+        <Router>
+          <Container></Container>
+        </Router>
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="*" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
