232e9dd09798039581a1fe6419e128c1e323cfc2
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-19 11:21:56 -0500

    created App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Box } from "@mui/material";
+import axios from "axios";
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:5500/api";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Box></Box>
+      </Router>
     </div>
   );
 }
