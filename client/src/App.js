ea66ea5a4dc034c5197399b19b23979172511703
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-19 15:57:09 -0500

    imported RightBar and LeftBar components

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import { useContext } from "react";
 import { InfoContext } from "./utility/InfoProvider";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
+import RightBar from "./components/RightBar";
+import LeftBar from "./components/LeftBar";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:5500/api";
@@ -20,24 +22,36 @@ function App() {
 
   return (
     <div className="App">
-      {authorized ? (
-        <Router>
-          <Navbar />
-          <Container>
+      <Box bgcolor={"background.default"} color="text.primary">
+        {authorized ? (
+          <Router>
+            <Navbar />
+            <Container>
+              <Grid sx={{ pt: { md: 3, sm: 2 } }} container spacing={3}>
+                <Grid item sm={3}>
+                  <LeftBar />
+                </Grid>
+                <Grid item sm={6} xs={12}>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                  </Routes>
+                </Grid>
+                <Grid>
+                  <RightBar />
+                </Grid>
+              </Grid>
+            </Container>
+          </Router>
+        ) : (
+          <Router>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="*" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
-          </Container>
-        </Router>
-      ) : (
-        <Router>
-          <Routes>
-            <Route path="*" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Router>
-      )}
+          </Router>
+        )}
+      </Box>
     </div>
   );
 }
