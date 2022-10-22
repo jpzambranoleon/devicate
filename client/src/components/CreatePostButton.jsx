7b26e2fea6a0ac3b06de42416b29d6f003fb947c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-21 20:24:14 -0500

    created CreatePostButton.jsx

---
+++ client/src/components/CreatePostButton.jsx
@@ -0,0 +1,109 @@
+import {
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Modal,
+  Stack,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
+
+const CreatePostButton = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen} variant="contained" fullWidth size="large">
+        Create Post
+      </Button>
+      <StyledModal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          width={400}
+          height={280}
+          bgcolor={"background.default"}
+          color="text.primary"
+          p={3}
+          borderRadius={5}
+        >
+          <Typography variant="h6" color="gray" align="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="/assets/person/diana.jpg"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="span">
+              Diana Ayi
+            </Typography>
+          </UserBox>
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            fullWidth
+            rows={3}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <IconButton aria-label="add emoji" color="primary">
+              <EmojiEmotions color="primary" />
+            </IconButton>
+            <IconButton aria-label="add image" color="secondary">
+              <Image color="secondary" />
+            </IconButton>
+            <IconButton aria-label="add video" color="success">
+              <VideoCameraBack color="success" />
+            </IconButton>
+            <IconButton aria-label="add person" color="error">
+              <PersonAdd color="error" />
+            </IconButton>
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="contained primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
+    </div>
+  );
+};
+
+export default CreatePostButton;
