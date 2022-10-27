2cbf9b8a663985fbd3018a307184792a6a923c77
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-26 22:07:52 -0500

    imported Rightbar and Leftbar to Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import LeftBar from "../../components/LeftBar";
 import Navbar from "../../components/Navbar";
+import RightBar from "../../components/RightBar";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Box sx={{ bgcolor: "background.paper" }}>
-        <Container>
-          <Typography>Home Page</Typography>
+        <Navbar />
+        <Container sx={{ mt: 2 }}>
+          <Grid container spacing={3}>
+            <Grid item sm={3}>
+              <LeftBar />
+            </Grid>
+            <Grid item sm={6} xs={12}>
+              <Typography>Feed</Typography>
+            </Grid>
+            <Grid item sm={3}>
+              <RightBar />
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </main>
