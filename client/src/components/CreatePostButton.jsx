3ec567718fa8107e6322e687aad98ead1c286d7a
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-26 22:06:53 -0500

    imported useEffect and axios

--- client/src/components/CreatePostButton.jsx
+++ client/src/components/CreatePostButton.jsx
@@ -10,14 +10,17 @@ import {
   Box,
   Button,
   ButtonGroup,
+  CardMedia,
   IconButton,
   Modal,
+  Skeleton,
   Stack,
   styled,
   TextField,
   Typography,
 } from "@mui/material";
-import { useRef } from "react";
+import axios from "axios";
+import { useEffect, useRef } from "react";
 import { useContext } from "react";
 import { useState } from "react";
 import { InfoContext } from "../utility/InfoProvider";
@@ -36,19 +39,60 @@ const UserBox = styled(Box)({
 });
 
 const CreatePostButton = () => {
-  const { setStatus, authorizedUser } = useContext(InfoContext);
+  const { setStatus } = useContext(InfoContext);
+  const userId = localStorage.getItem("user");
   const [open, setOpen] = useState(false);
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const desc = useRef();
   const [file, setFile] = useState(null);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES;
 
-  const submitHandler = async (e) => {
-    e.prventDefault();
-    const newPost = {
-      userId: authorizedUser._id,
-      desc: desc.current.value,
+  const [formData, setFormData] = useState({
+    userId: "",
+    desc: "",
+  });
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?userId=${userId}`);
+      setUser(res.data);
+      setLoading(false);
     };
+    fetchUser();
+  }, [userId]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(file);
+
+    formData.userId = userId;
+
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("image", file);
+      formData.img = fileName;
+
+      axios.post("/upload", data);
+    }
+    axios
+      .post("/posts/submit", formData)
+      .then((res) => {
+        setStatus({
+          open: true,
+          message: res.data.message,
+          severity: "success",
+        });
+        window.location.reload();
+      })
+      .catch((err) => {
+        let message = err.response ? err.response.data.message : err.message;
+        setStatus({ open: true, message: message, severity: "error" });
+      });
   };
 
   return (
@@ -74,28 +118,55 @@ const CreatePostButton = () => {
             Create Post
           </Typography>
           <UserBox>
-            <Avatar
-              src="/assets/person/diana.jpg"
-              sx={{ width: 30, height: 30 }}
-            />
+            {loading ? (
+              <Skeleton variant="circular" width={30} height={30} />
+            ) : (
+              <Avatar
+                src={
+                  !user.profilePic ? "/broken-image.jpg" : PF + user.profilePic
+                }
+                sx={{ width: 30, height: 30 }}
+              />
+            )}
             <Typography fontWeight={500} variant="span">
-              Diana Ayi
+              {user.firstName + " " + user.lastName}
             </Typography>
           </UserBox>
           <TextField
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, desc: e.target.value }))
+            }
             id="desc"
-            href={desc}
             multiline
             fullWidth
             rows={3}
             placeholder="What's on your mind?"
             variant="standard"
           />
+          {file && (
+            <CardMedia
+              component="img"
+              image={URL.createObjectURL(file)}
+              alt=""
+            />
+          )}
           <Stack direction="row" gap={1} mt={2} mb={3}>
             <IconButton aria-label="add emoji" color="primary">
               <EmojiEmotions color="primary" />
             </IconButton>
-            <IconButton aria-label="add image" color="secondary">
+            <IconButton
+              aria-label="add image"
+              color="secondary"
+              component="label"
+            >
+              <input
+                hidden
+                accept=".png, .jpg, .jpeg"
+                type="file"
+                name="img"
+                id="img"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
               <Image color="secondary" />
             </IconButton>
             <IconButton aria-label="add video" color="success">
@@ -110,7 +181,7 @@ const CreatePostButton = () => {
             variant="contained"
             aria-label="contained primary button group"
           >
-            <Button>Post</Button>
+            <Button onClick={handleSubmit}>Post</Button>
             <Button sx={{ width: "100px" }}>
               <DateRange />
             </Button>
