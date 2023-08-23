5a7a984d81ab5150fb45b7fdef8e2b23a00135bd
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-23 10:26:00 -0500

    modified file

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,5 +1,9 @@
 const Button = ({ text }) => {
-  return <button className="button">{text}</button>;
+  return (
+    <button className="button">
+      <span className="text">{text}</span>
+    </button>
+  );
 };
 
 export default Button;
