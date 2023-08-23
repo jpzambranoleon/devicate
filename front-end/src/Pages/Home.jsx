a84d431564f6011c031e23630b5a6187764488f4
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-22 21:32:55 -0500

    modified file

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,38 +1,54 @@
-import Card from "../components/Card";
 import Feed from "../components/Feed";
-import { Link } from "react-router-dom";
+import AppSocials from "../components/AppSocials";
+import { Avatar, AvatarGroup } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="home container">
-      <div className="home-container grid">
+    <div className="home section">
+      <div className="home-container container grid">
         <div className="left">
           <Feed />
+          <button>Load more</button>
         </div>
-        <div className="right">
+        <div
+          className="right"
+          style={{ display: "flex", flexDirection: "column", gap: "1.8rem" }}
+        >
           <div className="app-card">
             <div className="app-card__container">
               <div className="top">
-                <Link className="logo">Devicate</Link>
+                <span>Connect with developers</span>
               </div>
               <small className="text">
-                Become a better React developer. Following our tips, tricks, and
-                real life experiences.
+                Become a better developer. Follow and contribute to projects.
+                Connect with other developers in the field.
               </small>
-              <ul className="list">
-                <li className="item">
-                  <Link className="link">
-                    <i className="fa-brands fa-linkedin" />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="link">
-                    <i className="fa-brands fa-instagram" />
-                  </Link>
-                </li>
-              </ul>
+
+              <AvatarGroup className="avatar-group">
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://mui.com/static/images/avatar/1.jpg"
+                />
+                <Avatar
+                  alt="Travis Howard"
+                  src="https://mui.com/static/images/avatar/2.jpg"
+                />
+                <Avatar
+                  alt="Cindy Baker"
+                  src="https://mui.com/static/images/avatar/3.jpg"
+                />
+                <Avatar
+                  alt="Agnes Walker"
+                  src="https://mui.com/static/images/avatar/4.jpg"
+                />
+                <Avatar
+                  alt="Trevor Henderson"
+                  src="https://mui.com/static/images/avatar/6.jpg"
+                />
+              </AvatarGroup>
             </div>
           </div>
+          <AppSocials />
         </div>
       </div>
     </div>
