aa96fe1ff40613b2ce5f8627d7a9e7b3b7885124
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-22 21:22:43 -0500

    created Post.jsx

---
+++ front-end/src/components/Post.jsx
@@ -0,0 +1,33 @@
+import { format } from "timeago.js";
+
+const Post = ({ post }) => {
+  return (
+    <div className="post">
+      <img src={post.img} />
+      <div className="post-container">
+        <div className="post-container__content">
+          <div className="user">
+            <span className="date">{format(post.date)}</span>
+          </div>
+          <span
+            className="post-title"
+            style={{
+              fontSize: "22px",
+              fontWeight: 600,
+              marginBottom: "1rem",
+            }}
+          >
+            {post.title}
+          </span>
+          <p style={{ whiteSpace: "pre-line" }}>{post.desc}</p>
+        </div>
+        <div className="post-container__footer">
+          <i className="fa-regular fa-heart" />
+          <i class="fa-regular fa-bookmark"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
