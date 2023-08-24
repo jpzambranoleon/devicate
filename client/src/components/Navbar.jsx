45c9b4fe621d2d6f1a71c16100b8ed5b0b1f8a8f
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-23 23:54:33 -0500

    moved to new folder

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,157 +1,106 @@
-import { MenuTwoTone, SportsSoccer } from "@mui/icons-material";
-import {
-  AppBar,
-  Avatar,
-  Box,
-  Button,
-  IconButton,
-  Menu,
-  MenuItem,
-  Skeleton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import axios from "axios";
-import { useContext, useEffect } from "react";
+import { Switch, styled } from "@mui/material";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { InfoContext } from "../utility/InfoProvider";
-import TempDrawer from "./TempDrawer";
-import { useNavigate } from "react-router-dom";
+import React from "../assets/react.svg";
+import { Link, useNavigate } from "react-router-dom";
 
-const settings = ["Logout"];
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 32,
+    height: 32,
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
+  },
+}));
 
-const Navbar = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const { authorized } = useContext(InfoContext);
-  const userId = localStorage.getItem("user");
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
+const Navbar = ({ setDarkMode, darkMode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [checked, setChecked] = useState(darkMode);
+
   const navigate = useNavigate();
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES;
 
-  const handleDrawerOpen = () => {
-    setOpenDrawer(true);
+  const handleThemeChange = (e) => {
+    const isChecked = e.target.checked;
+    setChecked(isChecked);
+    setDarkMode(isChecked);
   };
 
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
+  const handleClick = () => {
+    navigate("/profile");
   };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const onContextMenuClick = (action) => {
-    setAnchorElUser(null);
-    if (action.toLowerCase() === "profile") {
-      console.log("profile");
-    } else if (action.toLowerCase() === "logout") {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      navigate("/");
-      window.location.reload();
-    }
-  };
-
-  useEffect(() => {
-    if (authorized) {
-      setLoading(true);
-      const fetchUser = async () => {
-        const res = await axios.get(`/users?userId=${userId}`);
-        setUser(res.data);
-        setLoading(false);
-      };
-      fetchUser();
-    }
-  }, [authorized, userId]);
 
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar
-          sx={{
-            width: { xl: "75%", lg: "90%" },
-            margin: { xl: "0 auto", lg: "0 auto" },
-            justifyContent: "space-between",
-          }}
+    <header className="header">
+      <nav className="nav container">
+        <div className="nav-left">
+          <div className="nav-left__logo">
+            <Link to="/">Devicate</Link>
+          </div>
+          <div className="nav-left__search">
+            <div className="nav-left__search__bar">
+              <i className="fa-solid fa-magnifying-glass" />
+              <input
+                type="search"
+                placeholder="Search..."
+                className="search-input"
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          className="nav-right"
+          style={{ display: "flex", alignItems: "center", gap: "1rem" }}
         >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Typography color="inherit" variant="h5" noWrap>
-              Devicate
-            </Typography>
-          </Box>
-          <Box>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <Button component={Link} to="/" color="inherit">
-                Home
-              </Button>
-              <Button component={Link} to="/about" color="inherit">
-                About
-              </Button>
-            </Box>
-          </Box>
-          <Box>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <IconButton sx={{ ml: 2, p: 0 }} onClick={handleOpenUserMenu}>
-                {loading ? (
-                  <Skeleton variant="circular" width={35} height={35} />
-                ) : (
-                  <Avatar
-                    sx={{ height: 35, width: 35 }}
-                    src={
-                      !user.profilePic
-                        ? "/broken-image.jpg"
-                        : PF + user.profilePic
-                    }
-                  />
-                )}
-              </IconButton>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <MenuItem
-                  component={Link}
-                  to={`/${user.username}`}
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">Profile</Typography>
-                </MenuItem>
-                {settings.map((setting) => (
-                  <MenuItem
-                    key={setting}
-                    onClick={() => onContextMenuClick(setting)}
-                  >
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <IconButton
-              sx={{ display: { xs: "flex", sm: "none" } }}
-              onClick={handleDrawerOpen}
-            >
-              <MenuTwoTone />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </>
+          <MaterialUISwitch
+            checked={checked}
+            onChange={handleThemeChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+          <i className="fa-solid fa-user" onClick={handleClick} />
+          <button className="menu-button">
+            <i className="fa-solid fa-bars" />
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- client/src/utility/convertTime.js
+++
@@ -1,7 +0,0 @@
-export default function convertTime(time) {
-  return new Date(time).toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-}
